41f3171aec102b4d7854f3a1e434c3e25dd92888
Minhaj <syedminhajalbeez@gmail.com>
2024-09-20 09:56:46 -0700

    issueagain

--- config/database.js
+++ config/database.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000, 
+
 })
 .then(() => console.log('Database connected'))
 .catch(err => console.error('Database connection error:', err));
--- routes/infoRoutes.js
+++ routes/infoRoutes.js
@@ -4,8 +4,18 @@ const infoController = require('../controller/infoController');
 const { validate } = require('../middlewares/validationMiddleware');
 const { infoSchema } = require('../schemas/validationSchemas');
 const upload = require('../config/cloudinaryConfig'); 
+const InfoModel = require('../models/Information');
 
-router.get('/getInfo', infoController.getAllInfo);
+// router.get('/getInfo', infoController.getAllInfo);
+
+router.get('/getInfo', async (req, res) => {
+  try {
+    const users = await InfoModel.find({});
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 router.post(
   '/createInfo', 
