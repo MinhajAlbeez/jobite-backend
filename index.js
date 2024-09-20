589067dc854c350a54786028ec77a0e3d8ce9d92
Minhaj <syedminhajalbeez@gmail.com>
2024-09-20 10:33:14 -0700

    routess

--- index.js
+++ index.js
@@ -6,19 +6,28 @@ const infoRoutes = require('./routes/infoRoutes');
 const { validate } = require('./middlewares/validationMiddleware');
 const { infoSchema } = require('./schemas/validationSchemas');
 const userRoutes = require('./routes/userRoutes');
+const InfoModel = require('./models/Information');
 
 app.use(cors());
 app.use(express.json());
 // app.use(express.json({ limit: '50mb' }));
 
 
-app.get('/', (req, res) => {
-    res.send(`server is running on portt ${PORT}`);
-  });
-  app.use('/api', userRoutes); 
+// app.get('/', (req, res) => {
+//     res.send(`server is running on portt ${PORT}`);
+//   });
+  app.use('/api', userRoutes)
 
 
-app.use('/info', infoRoutes);
+// app.use('/info', infoRoutes);
+app.get('/', async (req, res) => {
+  try {
+    const users = await InfoModel.find({});
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 const PORT = process.env.PORT || 8000;
 
